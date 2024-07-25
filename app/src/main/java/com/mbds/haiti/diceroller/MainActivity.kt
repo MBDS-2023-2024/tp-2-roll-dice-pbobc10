115d8272b39552210187ab16d322f0868f7eff25
pbobc10 <pbobc10@gmail.com>
2024-07-25 17:00:59 -0400

    Rendre le bouton interactif

--- app/src/main/java/com/mbds/haiti/diceroller/MainActivity.kt
+++ app/src/main/java/com/mbds/haiti/diceroller/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.mbds.haiti.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTexView : TextView = findViewById(R.id.textView)
+            resultTexView.text = "6"
         }
+
     }
 }
